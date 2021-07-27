b3003405af9df37201ba10efe346c112335291d3
Zachary K <zachwkrause@gmail.com>
2021-07-27 14:56:43 -0700

    added db

---
+++ public/service-worker.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/manifest.webmanifest',
+  '/public/assets/css/style.css',
+  '/public/assets/js/loadImages.js',
+  '/public/assets/images/icons/icon-192x192.png',
+  '/public/assets/images/icons/icon-512x512.png',
+  '/public/db.js'
+];
+
+//install and open cache files
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Files successfully pre-cached!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+//gets keys for cache and updates changes
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+
+//fetch and offline checking / cache loading
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
